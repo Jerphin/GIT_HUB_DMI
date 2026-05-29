4ad5258a8111129c606cae846ea223179c74933d
Jerphin Asmi <jerphinasmi24@gmail.com>
2026-05-29 13:13:15 +0530

    new file added

---
+++ addition.py
@@ -0,0 +1,12 @@
+class square:
+    def __init__(self):
+        self.a=int(input("Enter the value:"))
+    def abc(self):
+        self.b=self.a**2
+        print(self.b)
+    def xyz(self):
+        self.c=self.a**3
+        print(self.c)
+sq=square()
+sq.abc()
+sq.xyz()
